f4daebd4b1599320d5a7c65961ae9215cfaf3a9f
Joseph Lemus <127916370+JosephLemus@users.noreply.github.com>
2026-03-09 19:52:59 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-
-User = "standard_user"
-Password = "secret_sauce"
+USER = "standard_user"
+PASSWORD = "secret_sauce"
 
 def main():
     service = Service(ChromeDriverManager().install())
@@ -18,27 +17,35 @@ def main():
     option.add_argument("--window-size=1920,1080")
     driver = Chrome(service=service, options=option)
     driver.get("https://www.saucedemo.com/")
-#login
     user_input = driver.find_element(By.ID, "user-name")
-    user_input.send_keys(User)
+    user_input.send_keys(USER)
     pass_input = driver.find_element(By.ID, "password")
-    pass_input.send_keys(Password)
+    pass_input.send_keys(PASSWORD)
     button = driver.find_element(By.ID, "login-button")
     button.click()
-#extraerdatos
-    products = driver.find_elements(By.CLASS_NAME, "inventory_item")
+    time.sleep(5)
+
+    # Se usa CSS_SELECTOR porque CLASS_NAME no permite múltiples clases (espacios)
+    # Agregamos el punto (.) para que sea un selector de clase válido
+    products = driver.find_elements(By.CSS_SELECTOR, ".inventory_item")
     product_data = []
     for product in products:
-        name = product.find_element(By.CLASS_NAME, "inventory_item_name").text
-        price = product.find_element(By.CLASS_NAME, "inventory_item_price").text
-        print(f"Nombre: {name}, Precio: {price}")
-        product_data.append([name, price])
+        try:
+            name = product.find_element(By.CLASS_NAME, "inventory_item_name").text
+            price = product.find_element(By.CLASS_NAME, "inventory_item_price").text
+           # print(f"Nombre: {name}, Precio: {price}")
+            product_data.append([name, price])
+        except Exception:
+            # Si no encuentra el precio en algún elemento, saltamos al siguiente
+            continue
 
-#cuadro con datos
+    driver.quit()
+
+    #cuadro con datos bonita
     import pandas as pd
     df = pd.DataFrame(product_data, columns=["Nombre", "Precio"])
     print(df)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
